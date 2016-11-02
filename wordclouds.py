9b71868d71c3612e0dfdc8b263ed0332fe179690
Julianna Stevenson <steve146@purdue.edu>
2016-11-01 22:07:50 -0400

    added finding nouns and adjectives

--- wordclouds.py
+++ wordclouds.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from os import path
+from nltk import *
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
@@ -9,8 +10,29 @@ d = path.dirname(__file__)
 maleText = open(path.join(d, 'male.txt')).read()
 femaleText = open(path.join(d, 'female.txt')).read()
 
+maleTokens = word_tokenize(maleText)
+femaleTokens = word_tokenize(femaleText)
+
+tagged_Male = pos_tag(maleTokens)
+tagged_Female = pos_tag(femaleTokens)
+
+male_words = ''
+female_words = ''
+
+# Generate lists of nouns and adjectives
+for x in tagged_Male:
+    if x[1] == 'NN' or x[1] == 'JJ':
+        male_words += ' '
+        male_words += x[0]
+
+for y in tagged_Female:
+    if y[1] == 'NN' or x[1] == 'JJ':
+        female_words += ' '
+        female_words += y[1]
+
+
 # Generate wordcloud images
-maleWordCloud = WordCloud().generate(maleText)
+maleWordCloud = WordCloud().generate(male_words)
 femaleWordCloud = WordCloud().generate(femaleText)
 
 # Display images
