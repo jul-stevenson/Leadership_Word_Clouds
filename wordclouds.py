32ee42a3d369f7c52fd1fbd4ee9e1ec0b65c653f
Julianna Stevenson <steve146@purdue.edu>
2016-11-03 22:18:13 -0700

    started implementation of counting different types of words and plotting totals

--- wordclouds.py
+++ wordclouds.py
@@ -5,6 +5,7 @@ from wordcloud import WordCloud
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 d = path.dirname(__file__)
 
@@ -21,16 +22,32 @@ tagged_Female = pos_tag(femaleTokens)
 male_words = ''
 female_words = ''
 
+label_numbers = [1, 2, 3, 4]
+labels = ["Male NN", "Male JJ", "Female NN", "Female JJ"]
+count = [0, 0, 0, 0]
+
 # Generate lists of nouns and adjectives
 for x in tagged_Male:
     if x[1] == 'NN' or x[1] == 'JJ':
         male_words += ' '
         male_words += x[0]
+        if x[1] == 'NN':
+            count[0] = count[0] + 1
+        else:
+            count[1] = count[1] + 1
 
 for y in tagged_Female:
     if y[1] == 'NN' or x[1] == 'JJ':
         female_words += ' '
         female_words += y[1]
+        if y[1] == 'NN':
+            count[2] = count[2] + 1
+        else:
+            count[3] = count[3] + 1
+
+plt.bar(label_numbers, count, align='center')
+plt.xticks(label_numbers, labels)
+plt.show()
 
 man_mask = np.array(Image.open(path.join(d, "man.jpg")))
 
