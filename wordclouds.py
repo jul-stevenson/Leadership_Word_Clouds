d6a2d77ee5e251be5f759d4405e10f68f49005ae
Julianna Stevenson <steve146@purdue.edu>
2016-11-04 14:46:46 -0400

    updated bar chart to display % instead of total types of words

--- wordclouds.py
+++ wordclouds.py
@@ -22,38 +22,57 @@ tagged_Female = pos_tag(femaleTokens)
 male_words = ''
 female_words = ''
 
-label_numbers = [1, 2, 3, 4]
-labels = ["Male NN", "Male JJ", "Female NN", "Female JJ"]
-count = [0, 0, 0, 0]
+label_numbers = [1, 2, 3, 4, 5, 6]
+labels = ["Male NN", "Male JJ", "Male VB", "Female NN", "Female JJ", "Female VB"]
+count = [0, 0, 0, 0, 0, 0]
+maleTotal = 0.0
+femaleTotal = 0.0
 
 # Generate lists of nouns and adjectives
 for x in tagged_Male:
-    if x[1] == 'NN' or x[1] == 'JJ':
+    maleTotal = maleTotal + 1
+    sub = x[1][:2]
+    if sub == 'NN' or sub == 'JJ' or sub == 'VB':
         male_words += ' '
         male_words += x[0]
-        if x[1] == 'NN':
+        if sub == 'NN':
             count[0] = count[0] + 1
-        else:
+        elif sub == 'JJ':
             count[1] = count[1] + 1
+        else:
+            count[2] = count[2] + 2
 
 for y in tagged_Female:
-    if y[1] == 'NN' or x[1] == 'JJ':
+    femaleTotal = femaleTotal + 1
+    sub = y[1][:2]
+    if sub == 'NN' or sub == 'JJ' or sub == 'VB':
         female_words += ' '
         female_words += y[1]
-        if y[1] == 'NN':
-            count[2] = count[2] + 1
-        else:
+        if sub == 'NN':
             count[3] = count[3] + 1
+        elif sub == 'JJ':
+            count[4] = count[4] + 1
+        else:
+            count[5] = count[5] + 1
 
+# Calculate percentages of words are nouns or adjectives
+count[0] = count[0]/maleTotal*100
+count[1] = count[1]/maleTotal*100
+count[2] = count[2]/femaleTotal*100
+count[3] = count[3]/femaleTotal*100
+
+# Show bar graph of noun and adj percentages
 plt.bar(label_numbers, count, align='center')
 plt.xticks(label_numbers, labels)
+plt.ylim([0,100])
 plt.show()
 
-man_mask = np.array(Image.open(path.join(d, "man.jpg")))
+#man_mask = np.array(Image.open(path.join(d, "man.jpg")))
 
 # Generate wordcloud images
-maleWordCloud = WordCloud(max_words=30, mask=man_mask).generate(male_words)
-femaleWordCloud = WordCloud(background_color="white", max_words=30).generate(femaleText)
+#maleWordCloud = WordCloud(max_words=30, mask=man_mask).generate(male_words)
+maleWordCloud = WordCloud(max_words = 100).generate(male_words)
+femaleWordCloud = WordCloud(background_color="white", max_words = 100).generate(femaleText)
 
 # Display images
 plt.imshow(maleWordCloud)
